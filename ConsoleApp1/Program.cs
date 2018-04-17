c3e25a3080936e8162b0b07c52caaa7583a0bf5d
Naoki Egawa <naokun0131@gmail.com>
2018-04-17 13:42:14 +0900

    プロジェクト ファイルを追加します。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("eee");
+            Debug.WriteLineIf(1 == 2, "test");
+
+        }
+    }
+}
